378caf83cf1c7dbfe71d9454af147846032c880d
Julien <julienmulle@gmail.com>
2021-09-07 10:43:44 +0200

    ajout prop-types pour Header

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+//composant creer
 import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
 import Footer from 'src/components/Footer';
-
+//données
+import categoriesData from 'src/data/categories';
+import postsData from 'src/data/posts';
+//style
 import './style.scss';
 
 function Blog() {
   return (
     <div className="blog">
-      <Header/>
+      <Header categories={categoriesData}/>
       <Posts/>
       <Footer/>
     </div>
