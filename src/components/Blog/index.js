46c337dfc3d36236f58bcf35a73fca5c7513fd7a
Julien <julienmulle@gmail.com>
2021-09-13 11:09:34 +0200

    utilisation requete axios

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 // composant créer
 import Header from 'src/components/Header';
@@ -17,6 +18,7 @@ function Blog() {
 
   const [posts, setPosts] = useState([]);
   const [loading,setLoading] = useState(false)
+  const [hasError, setHasError] = useState(false);
   // on va générer un composant Route pour chaque catégorie
   // ce composant Route viendra prendre une liste de poste triée en fonction
   // de la catégorie
@@ -34,19 +36,33 @@ function Blog() {
   });
   const loadData = () => {
     setLoading(true);
-    // on simule le chargement de données
-    setTimeout(() => {
-      console.log('ici je veux changer mon state');
-      setPosts(postsData);
+    axios.get('https://oclock-open-apis.vercel.app/api/blog/posts')
+    .then((response) => {
+      // axios nous renvoie un objet, les data de la réponse se trouvent dans la propriété "data"
+      setPosts(response.data);
+    })
+    .catch((error) => {
+      console.log('error', error);
+      setHasError(true);
+    })
+    .finally(() => {
       setLoading(false);
-    }, 1000);
+    });
+    // on simule le chargement de données
+   // setTimeout(() => {
+     // console.log('ici je veux changer mon state');
+      //setPosts(postsData);
+      //setLoading(false);
+    //}, 1000);
   };
 
   return (
     <div className="blog">
       <Header categories={categoriesData} />
       <button type="button" onClick={loadData}>Load data</button>
+      {hasError && <div>Une erreur s'est produite</div>}
       {loading && <div>Chargement des données</div>}
+      {!loading && !hasError && (
       <Switch>
         {routes}
         <Redirect from="/jquery" to="/autre" />
@@ -54,6 +70,7 @@ function Blog() {
           <NotFound />
         </Route>
       </Switch>
+      )}
       <Footer />
     </div>
   );
