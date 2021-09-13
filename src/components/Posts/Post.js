341108ab7ae82438d473d567660a863bca8662c0
Julien <julienmulle@gmail.com>
2021-09-13 11:31:07 +0200

    utilisation de dompurify

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import DOMPurify from 'dompurify';
 
 function Post({title, category, excerpt}) {
+    // lorqu'on récupère des données, il faut s'assurer qu'elles ne sont pas
+  // dangereuses. Pour cela il faut les assainir, ici avec DOMPurify
+  const createMarkup = () => {
+    const clean = DOMPurify.sanitize(excerpt, { ALLOWED_TAGS: ['em', 'strong'] });
+    return { __html: clean };
+  };
     return (
         <li className="post">
         <article>
           <h2 className="post__title">{title}</h2>
           <div className="post__category">{category}</div>
-          <p className="post__content">{excerpt}</p>
+          <p className="post__content"dangerouslySetInnerHTML={createMarkup()}/>
         </article>
       </li>
     );
