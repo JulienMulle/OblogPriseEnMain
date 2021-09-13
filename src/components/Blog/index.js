43f43cc938096fe70ebc47c7df3804a653cb3f27
Julien <julienmulle@gmail.com>
2021-09-13 09:55:02 +0200

    utilisation d'un usestate

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // composant créer
 import Header from 'src/components/Header';
@@ -14,11 +14,14 @@ import postsData from 'src/data/posts';
 import './style.scss';
 
 function Blog() {
+
+  const [posts, setPosts] = useState([]);
+  const [loading,setLoading] = useState(false)
   // on va générer un composant Route pour chaque catégorie
   // ce composant Route viendra prendre une liste de poste triée en fonction
   // de la catégorie
   const routes = categoriesData.map((category) => {
-    const postsList = getPostsByCategory(category.label, postsData);
+    const postsList = getPostsByCategory(category.label, posts);
     return (
       <Route
         path={category.route}
@@ -29,11 +32,24 @@ function Blog() {
       </Route>
     );
   });
+  const loadData = () => {
+    setLoading(true);
+    // on simule le chargement de données
+    setTimeout(() => {
+      console.log('ici je veux changer mon state');
+      setPosts(postsData);
+      setLoading(false);
+    }, 1000);
+  };
+
   return (
     <div className="blog">
       <Header categories={categoriesData} />
+      <button type="button" onClick={loadData}>Load data</button>
+      {loading && <div>Chargement des données</div>}
       <Switch>
         {routes}
+        <Redirect from="/jquery" to="/autre" />
         <Route>
           <NotFound />
         </Route>
